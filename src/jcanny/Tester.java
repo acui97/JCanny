1a86bdb3d9e927fe4ea2ba45636c3533fabef370
Andrew Cui <cui.400@osu.edu>
2020-04-03 00:32:29 -0400

    Modified an existing repository to enable edge detection processing on a wider scale

--- src/jcanny/Tester.java
+++ src/jcanny/Tester.java
@@ -24,7 +24,7 @@ import javax.imageio.ImageIO;
 /**
  * This class demonstrates the usage of the JCanny Canny edge detector library.
  * 
- * @author robert
+ * @author robert, andy
  */
 
 public class Tester {
@@ -33,30 +33,63 @@ public class Tester {
     private static final int CANNY_STD_DEV = 1;             //Range 1-3
     
     //I/O parameters
-    private static String imgFileName;
-    private static String imgOutFile = "";
+    private static String imgFolderName;
+    private static String imgOutFile;
     private static String imgExt;
 
     public static void main(String[] args) {
         //Read input file name and create output file name
-        imgFileName = args[0];
+        imgFolderName = args[0];
         imgExt = args[1];
-        String[] arr = imgFileName.split("\\.");
         
-        for (int i = 0; i < arr.length - 1; i++) {
-            imgOutFile += arr[i];
+        int fileCount = new File(imgFolderName).listFiles().length;
+        int counter = 1;
+        
+        long start;
+        long elapsed;
+        
+        for (final File fileEntry : new File(imgFolderName).listFiles()) {
+        	start = System.currentTimeMillis();
+        	
+        	imgOutFile = args[2];
+        	String[] arr = fileEntry.getName().split("\\.");
+            
+            for (int i = 0; i < arr.length - 1; i++) {
+                imgOutFile += arr[i];
+            }
+            
+            imgOutFile += "_canny.";
+            imgOutFile += imgExt;
+            
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+            	//Sample JCanny usage
+                try {
+                    BufferedImage input = ImageIO.read(fileEntry);
+                    BufferedImage output = JCanny.CannyEdges(input, CANNY_STD_DEV, CANNY_THRESHOLD_RATIO);
+                    ImageIO.write(output, imgExt, new File(imgOutFile));
+                } catch (Exception ex) {
+                    System.out.println("ERROR ACCESING IMAGE FILE:\n" + ex.getMessage());
+                }
+            }
+            
+            elapsed = System.currentTimeMillis() - start;
+            
+            System.out.println("Completed " + counter + " out of " + fileCount + " images. That image took " + elapsed + "ms. File created: " + imgOutFile);
+            
+            counter++;
         }
         
-        imgOutFile += "_canny.";
-        imgOutFile += imgExt;
-        
-        //Sample JCanny usage
-        try {
-            BufferedImage input = ImageIO.read(new File(imgFileName));
-            BufferedImage output = JCanny.CannyEdges(input, CANNY_STD_DEV, CANNY_THRESHOLD_RATIO);
-            ImageIO.write(output, imgExt, new File(imgOutFile));
-        } catch (Exception ex) {
-            System.out.println("ERROR ACCESING IMAGE FILE:\n" + ex.getMessage());
+    }
+    
+    public static void listFilesForFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getName());
+            }
         }
-    }    
+    }
 }
